17bb36133f205945608c91fde344a9e3f8bb0b0f
Chelsea Smith <smith2chelsea2@gmail.com>
2021-05-12 19:30:52 -0400

    created questions script

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,4 @@
-var q = [
+/*var q = [
     {
         title: "what color is the sky"
         choices: ["black, green, blue, yellow"]
@@ -9,6 +9,32 @@ var q = [
 {
     title: "who makes the m3"
     choices: ["honda", "audi", "mercedes"]
-    answer: "blue"
-}
-]
+    answer: "blue"*/
+
+    var myQuestions = [
+        {
+            title: "How many years can a snail sleep for?",
+            choices: ["3, 1, < 1, 7"],
+            answer: "3",
+        },
+        {
+            title: "What is the only animal that cannot jump?",
+            choices: ["Rhino, Hippo, Elephant, Alligator"],
+            answer: "Elephant"
+        },
+        {
+            title: "What is a rhino's horn made out of?",
+            choices: ["Bone, Hair, Cartilage, Enamel"],
+            answer: "Hair",
+        },
+        {
+            title: "About how many glasses of milk can a cow make in its lifetime?",
+            choices: ["500, 200000, 3000, 1000000"],
+            answer: "200000",
+        },
+        { 
+            title: "An ostrich's eye is bigger than its ____",
+            choices: ["Brain, Heart, Stomach, Beak"],
+            answer: "Brain",
+        },
+    ]
