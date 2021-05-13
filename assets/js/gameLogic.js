831d0824085cd16ba88698b75e4042beb5eaea57
Chelsea Smith <smith2chelsea2@gmail.com>
2021-05-12 22:23:36 -0400

    js beginings

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,74 +1,30 @@
-// declare variable at top state
+var startButton = document.getElementById("start-button")
+var myQuestions = [
+    {
+        title: "How many years can a snail sleep for?",
+        choices: ["3, 1, < 1, 7"],
+        answer: "3",
+    },
+    {
+        title: "What is the only animal that cannot jump?",
+        choices: ["Rhino, Hippo, Elephant, Alligator"],
+        answer: "Elephant"
+    },
+    {
+        title: "What is a rhino's horn made out of?",
+        choices: ["Bone, Hair, Cartilage, Enamel"],
+        answer: "Hair",
+    },
+    {
+        title: "About how many glasses of milk can a cow make in its lifetime?",
+        choices: ["500, 200000, 3000, 1000000"],
+        answer: "200000",
+    },
+    {
+        title: "An ostrich's eye is bigger than its ____",
+        choices: ["Brain, Heart, Stomach, Beak"],
+        answer: "Brain",
+    },
+]
 
-//declare varibale to keep track of our quiz state (questions index, times, time variables)
-
-//variable to reference DOM elements
-//var question = document.getElementById("your id goes here")
-
-//create a startquiz function to start your quiz
-// put timer stuff in her an run your function to get your quiz items
-
-// create a function to render questions for the user 
-function get(){
-    // get current question object from the array 
-
-    //update your html with current question
-
-    //clear our any older question choices
-
-    //loop over our choices and crate new button for new choices
-        // check out. forEach()
-
-    // attach click event listener to each choice
-    choice.onclick = qclick;
-
-    //display questions on the page 
-}
-
-
-// create a function for a questionClicked()
-function = qClick (){
-    // check if user answered wrong question
-    // if you want you can decrement time if user is long 
-
-    // creat if to check it time has hit 0 or time is still remaining
-    // if (time < 0){
-         //   time = 0
-    //}
-
-
-    // display time on the page 
-
-    // check to see if the answer is correct if correct move on give gif (whatver your heart desires)
-    // move on to next question 
-
-    // check to see if we're out of questions
-    // if we are out of q's run endQuiz function else get next questins 
-    if (currentQIndex === questions.length){
-        quizEnd();
-    } else{
-        qutQ();
-    }
-
-    // create function to end quiz
-    // clear time interval
-    // show an end quiz page if you want
-    // show user final score 
-
-    // checl out methods.hide() and .show()
-
-    // function to run time 
-    // if time hits 0 run endQ()
-
-    // create function for highscore 
-
-    // write any function to check how many questions user got right and perform your math or hardcoded data to display user score
-
-    // chekc to make sure user inputs user name/intials into user prompt
-    // save high schores to local storage
-    // retrive high schores from local storage
-
-    // creare a new object to store user scores for the high score page 
-
-    // redirect to the highscores page
-    // HINT window.location.href
+startButton.addEventListener("click")
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,40 +1,27 @@
-/*var q = [
+var myQuestions = [
     {
-        title: "what color is the sky"
-        choices: ["black, green, blue, yellow"]
-        answer: "blue"
-    }
+        title: "How many years can a snail sleep for?",
+        choices: ["3, 1, < 1, 7"],
+        answer: "3",
+    },
+    {
+        title: "What is the only animal that cannot jump?",
+        choices: ["Rhino, Hippo, Elephant, Alligator"],
+        answer: "Elephant"
+    },
+    {
+        title: "What is a rhino's horn made out of?",
+        choices: ["Bone, Hair, Cartilage, Enamel"],
+        answer: "Hair",
+    },
+    {
+        title: "About how many glasses of milk can a cow make in its lifetime?",
+        choices: ["500, 200000, 3000, 1000000"],
+        answer: "200000",
+    },
+    {
+        title: "An ostrich's eye is bigger than its ____",
+        choices: ["Brain, Heart, Stomach, Beak"],
+        answer: "Brain",
+    },
 ]
-
-{
-    title: "who makes the m3"
-    choices: ["honda", "audi", "mercedes"]
-    answer: "blue"*/
-
-    var myQuestions = [
-        {
-            title: "How many years can a snail sleep for?",
-            choices: ["3, 1, < 1, 7"],
-            answer: "3",
-        },
-        {
-            title: "What is the only animal that cannot jump?",
-            choices: ["Rhino, Hippo, Elephant, Alligator"],
-            answer: "Elephant"
-        },
-        {
-            title: "What is a rhino's horn made out of?",
-            choices: ["Bone, Hair, Cartilage, Enamel"],
-            answer: "Hair",
-        },
-        {
-            title: "About how many glasses of milk can a cow make in its lifetime?",
-            choices: ["500, 200000, 3000, 1000000"],
-            answer: "200000",
-        },
-        { 
-            title: "An ostrich's eye is bigger than its ____",
-            choices: ["Brain, Heart, Stomach, Beak"],
-            answer: "Brain",
-        },
-    ]
