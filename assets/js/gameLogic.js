f37c21b4e5af66352b18255fd0df0420504c3d19
Chelsea Smith <smith2chelsea2@gmail.com>
2021-06-03 22:39:51 -0400

    choices buttons, timer changes, feedback

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,6 +1,36 @@
+
+
+const myQuestions = [
+    {
+        title: "How many years can a snail sleep for?",
+        choices: ["3", "1", "< 1", "7"],
+        answer: "3",
+    },
+    {
+        title: "What is the only animal that cannot jump?",
+        choices: ["Rhino", "Hippo", "Elephant", "Alligator"],
+        answer: "Elephant"
+    },
+    {
+        title: "What is a rhino's horn made out of?",
+        choices: ["Bone", "Hair", "Cartilage", "Enamel"],
+        answer: "Hair",
+    },
+    {
+        title: "About how many glasses of milk can a cow make in its lifetime?",
+        choices: ["500", "200000", "3000", '1000000'],
+        answer: "200000",
+    },
+    {
+        title: "An ostrich's eye is bigger than its ____",
+        choices: ["Brain", "Heart", "Stomach", "Beak"],
+        answer: "Brain",
+    },
+]
+
 // variables for quiz state
 var currentQuestionIndex = 0;
-var time = questions.length * 15;
+var time = myQuestions.length * 15;
 var timerId;
 
 // variables for DOM elements
@@ -15,9 +45,9 @@ var intialEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 var endscreenEl = document.getElementById("end-screen");
 
+   // hide endscreen
+    endscreenEl.style.visibility = "hidden";
 
-// hide endscreen
-endscreenEl.style.visibility = "hidden";
 
 function startQuiz() {
     // hide start screen when button clicked
@@ -28,7 +58,7 @@ function startQuiz() {
     else {
         startscreenEl.style.display = "none";
     }
-
+ 
 
     //start timer 
     // 1. call setInterval method, passing in a callback function and an interval value of 1000 as input arguments
@@ -51,19 +81,88 @@ function askQuestions() {
 
     choicesEl.innerHTML = "";
 
-    for (var i = 0; i < currentQuestion.choices.length; i++) {
-        var currentQuestionChoice = currentQuestion
-        currentQuestionChoice = document.createElement("button");
+    currentQuestion.choices.forEach(function(choice,i){
+        var choiceBtn= document.createElement("button");
+        choiceBtn.setAttribute("class", "choice-button");
+        choiceBtn.setAttribute("value", choice);
+        choiceBtn.textContent = i + 1 + ". " + choice;
+        choiceBtn.onclick= userChoice;
+        choicesEl.append(choiceBtn);
+    })
 
-        currentQuestionChoice.setAttribute("choice");
 
-        button.textContent = choices;
 
-        button.addEventListener("click" function(userChoices))
-    }
+
+
+
+
+
+
+    // for (var i = 0; i < currentQuestion.choices.length; i++) {
+    //     console.log(currentQuestion.choices)
+        
+    //     // currentQuestion.choices[0]= document.createElement("button");
+    //     // choicesEl.textContent = choices
+    //     // console.log(choices)
+        
+
+    // }
 }
 
-function userChoices () {}
+function userChoice(){
 
+    // USER CHOICE EVENT CALLBACK FUNCTION
+
+    // CHECK IF USER CHOICE IS WRONG
+    // 1. event target has the choice button element on which user clicked
+    //    that button element has the value attribute to contain the choice user clicked
+    //    console.log the event target to look for the where the value attribute is
+    // 2. get the anwer to the current question
+    // 3. compare these two variables to check if user choice is wrong
+  
+    if (this.value !== myQuestions[currentQuestionIndex].answer) {
+      // PENALIZE TIME BY SUBTRACTING 15 SECONDS FROM THE GLOBAL TIME VARIABLE
+        time -= 15;
+      // IF TIME IS LESS THAN 0, MAKE IT EQUAL TO 0
+        if (time < 0){
+            time = 0
+        };
+  
+      // DISPLAY NEW TIME ON THE PAGE BY ASSIGNING TIME TO TEXT CONTEXT OF HTML TIMER ELEMENT
+      timerEl.textContent = time
+  
+      // ASSIGN  "WRONG!" TO TEXT CONTENT OF HTML FEEDBACK ELEMENT
+      feedbackEl.textContent = "Wrong!";
+  
+    } else {
+      
+  
+      // ASSIGN "CORRECT!" TO TEXT CONTENT OF HTML FEEDBACK ELEMENT
+      feedbackEl.textContent = "Correct!";
+    }
+  
+    // FLASH RIGHT/WRONG FEEDBACK ON PAGE FOR HALF A SECOND
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    // MOVE TO NEXT QUESTION BY INCREMENTING THE GLOBAL INDEX VARIABLE
+    currentQuestionIndex++;
+    // CHEKC IF WE'VE RUN OUT OF QUESTIONS
+    // 1. if we've run out, call quizEnd function
+    // 2. else call ask questions funciton
+  if (currentQuestionIndex=myQuestions.length){
+    quizEnd();
+  }
+  else {
+    askQuestions();
+  }
+  }
+
+
+
+startBtn.onclick= startQuiz;
 
 function clockTick() {}
+function quizEnd() {}
--- assets/js/hsLogic.js
+++ assets/js/hsLogic.js
@@ -23,7 +23,7 @@ function printHighscores() {
     window.location.reload();
   }
   
-  document.getElementById("clear").onclick = clearHighscores;
+ // document.getElementById("clear").onclick = clearHighscores;
   
   // run function when page loads
   printHighscores();
